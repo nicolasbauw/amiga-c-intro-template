f75ec0f3d49c50f13611efc26bcac5c090a71d24
Nicolas BAUW <nbauw@hotmail.com>
2020-10-08 22:37:57 +0200

    Module to play is in a define

--- modplay.c
+++ modplay.c
@@ -1,13 +1,15 @@
 #include <proto/exec.h>
 #include "include/ptreplay.h"
 
+#define MODULE "assets/red.mod"
+
 struct Library *PTReplayBase;
 struct Module *Mod = NULL;
 BYTE SigBit;
 
 int mod_play() {
 	if((PTReplayBase = OpenLibrary("ptreplay.library",0L)) && (SigBit=AllocSignal(-1)!=-1)) {
-		if(Mod = PTLoadModule("assets/red.mod")) {
+		if(Mod = PTLoadModule(MODULE)) {
 			PTInstallBits(Mod, SigBit, -1, -1, -1);
 			PTPlay(Mod);
 			return 0;
