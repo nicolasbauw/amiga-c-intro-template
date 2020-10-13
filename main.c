002e5e30d9f2a450bf98faa9a19744dd5753ce00
Nicolas BAUW <nbauw@hotmail.com>
2020-10-13 12:03:39 +0200

    Indentation

--- main.c
+++ main.c
@@ -167,21 +167,21 @@ struct Module *Mod = NULL;
 BYTE SigBit;
 
 int mod_play() {
-	if((PTReplayBase = OpenLibrary("ptreplay.library",0L)) && (SigBit=AllocSignal(-1)!=-1)) {
-		if(Mod = PTLoadModule(MODULE)) {
-			PTInstallBits(Mod, SigBit, -1, -1, -1);
-			PTPlay(Mod);
-			return 0;
-		}
-	return 1;
-	}
+    if((PTReplayBase = OpenLibrary("ptreplay.library",0L)) && (SigBit=AllocSignal(-1)!=-1)) {
+        if(Mod = PTLoadModule(MODULE)) {
+            PTInstallBits(Mod, SigBit, -1, -1, -1);
+            PTPlay(Mod);
+            return 0;
+        }
+    return 1;
+    }
 }
 
 void mod_stop() {
-	PTStop(Mod);
-	FreeSignal(SigBit);
-	PTUnloadModule(Mod);
-	CloseLibrary(PTReplayBase);
+    PTStop(Mod);
+    FreeSignal(SigBit);
+    PTUnloadModule(Mod);
+    CloseLibrary(PTReplayBase);
 }
 #endif
 
