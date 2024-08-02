d9e528d2dcac469290a577c768c862660c051223
Nicolas BAUW <nbauw@hotmail.com>
2024-08-02 20:51:41 +0200

    Removed library stuff

--- make_adf.sh
+++ make_adf.sh
@@ -1,8 +1,4 @@
 #!/bin/zsh
 [ -f intro.adf ] || xdftool intro.adf format Intro ffs + boot install
 xdftool intro.adf delete intro
-xdftool intro.adf makedir libs
-xdftool intro.adf makedir assets
-xdftool intro.adf write assets/RED.MOD assets
-xdftool intro.adf write libs/ptreplay.library libs
 xdftool intro.adf write intro
